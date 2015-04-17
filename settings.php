3c7f5ad9c2c804b9bbd1ed4cfb7aa790522cb944
Emir <emiratesbp@gmail.com>
2015-04-17 15:21:39 +0200

    first commit

---
+++ settings.php
@@ -0,0 +1,41 @@
+<?php
+	$adresa[0]="6821 Benjamin Franklin Drive"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="Columbia, MD 21046";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="Columbia";
+	$telephone[0]=4109107800;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="1500 Lakeside Parkway Ste 100"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Flower Mound, TX 75028";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Texas";
+	$telephone[1]=4697135300;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="9255 SW Nimbus Ave"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Beaverton, OR 97008";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Oregon";
+	$telephone[2]=15034034700;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="7700 Irvine Center Dr, Ste 100"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Irvine, CA 92618";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="California";
+	$telephone[3]=9498857000;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="6821 Benjamin Franklin Drive"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Columbia, MD 21046";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="Maryland";
+	$telephone[4]=4109107800;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="1 Hines Road, Suite 100"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="Kanata, Ontario K2K 3C7";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="Canada";
+	$telephone[5]=6135928000;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="slideup"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
